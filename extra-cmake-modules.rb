25333593adbf240f922c73a2d03db9f8f246b1c5
Martin Müller <mail@mueller-martin.net>
2018-04-09 16:56:45 +0200

    extra-cmake-modules: Update and fix deprecation

--- extra-cmake-modules.rb
+++ extra-cmake-modules.rb
@@ -1,12 +1,12 @@
 class ExtraCmakeModules < Formula
   desc "Extra modules and scripts for CMake"
   homepage "https://github.com/KDE/extra-cmake-modules"
-  url "https://github.com/KDE/extra-cmake-modules/archive/v5.28.0.tar.gz"
-  sha256 "e70f478785be6ab338f4ae2a4c2235ad9f5cb448e5b38aeffde983f10baae431"
+  url "https://github.com/KDE/extra-cmake-modules/archive/v5.36.0.tar.gz"
+  sha256 "626df3f4dc7af7c3688377917a9f473f85354fc8ed16c5bb7c99a1f375e54dc9"
 
-  depends_on "cmake" => :run
-  depends_on "qt5" => :optional
   depends_on "sphinx-doc" => :build
+  depends_on "cmake"
+  depends_on "qt5" => :optional
 
   def install
     args = std_cmake_args
